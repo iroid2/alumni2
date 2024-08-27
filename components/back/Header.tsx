0feb244b2493b9b4c43589ae748027a7ac15e7ee
iroid <iradtu2@gmail.com>
2024-08-27 17:29:58 +0300

    sorted the s.e.o robots error

--- components/back/Header.tsx
+++ components/back/Header.tsx
@@ -66,7 +66,7 @@ export default function Header() {
                   className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                 >
                   <Home className="h-5 w-5" />
-                  Dashboard
+                  Dashboards
                 </Link>
                 <Link
                   href="#"
